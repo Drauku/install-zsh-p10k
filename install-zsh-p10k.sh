3228a50f1fb8ae26d708254650afc8761dcd3b06
Drauku <drauku@gmail.com>
2022-12-30 00:31:45 -0600

    add get.docker to install script

--- install-zsh-p10k.sh
+++ install-zsh-p10k.sh
@@ -1,27 +1,29 @@
-# a script to install zsh, oh-my-zsh, powerlevel10k, meslo lgs fonts and several zsh plugins
+#!/bin/bash
+## a script to install zsh, oh-my-zsh, powerlevel10k, meslo lgs fonts, zsh plugins
+## docker-ce will be installed, as well as adding a 'docker' user and 'docker' group
 
-## install zsh
-sudo apt install -y curl fontconfig git zsh
-
-## install oh-my-zsh
-echo "YOU MUST ACCEPT 'zsh' AS THE NEW SHELL, AND THEN TYPE `exit` ONCE oh-my-zsh IS FINISHED INSTALLING
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+## install required apps
+apt update -y && apt upgrade -y && apt install -y curl fontconfig git zsh
 
 ## download and install necessary fonts
-mkdir -p ~/.fonts && cd ~/.fonts
+mkdir -p ~/.fonts && cd ~/.fonts || return
 curl -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
 curl -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
 curl -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
 curl -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 fc-cache -f -v
 
+## install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+echo "ONCE 'oh-my-zsh' IS FINISHED INSTALLING, TYPE 'exit' TO CONTINUE THIS SCRIPT"
+
 ## download powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 ## download zsh-z zsh-autosuggestions zsh-syntax-highlighting plugin
-git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
 ## backup original .zshrc and set theme, plugins, and auto load the .p10k.zsh on terminal session start
 [[ ! -f ~/.zshrc~ ]] || cp ~/.zshrc ~/.zshrc~
@@ -32,14 +34,26 @@ sed -i '1i if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-
 sed -i '1i # confirmations, etc.) must go above this block; everything else may go below.' ~/.zshrc
 sed -i '1i # Initialization code that may require console input (password prompts, [y/n]' ~/.zshrc
 sed -i '1i # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.' ~/.zshrc
-echo '\nPOWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true\n' >> ~/.zshrc
-echo '# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.' >> ~/.zshrc
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+{
+  printf "\nPOWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true\n";
+  printf "\n# To customize prompt, run 'p10k configure' or edit ~/.p10k.zsh.\n";
+  printf "\n[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh";
+} >> ~/.zshrc
 
 ## backup original .p10k.conf and replace with drauku's custom .p10k.conf
-wget https://raw.githubusercontent.com/Drauku/install-zsh-p10k/main/.p10k.zsh.custom
+curl -fs https://raw.githubusercontent.com/Drauku/install-zsh-p10k/main/.p10k.zsh.custom -o ~/.p10k.zsh.custom
 [[ ! -f ~/.p10k.zsh.original ]] || cp ~/.p10k.zsh ~/.p10k.original
 cp --backup ~/.p10k.zsh.custom ~/.p10k.zsh
+
+# ## add queried user
+# while read -r -p " What 'username' you would like assigned as the first UID/GID? " username; do
+#   # First Last,RoomNumber,WorkPhone,HomePhone
+#   adduser "${username}" --gecos ",,," --disabled-password
+#   echo "${username}:password" | sudo chpasswd
+# done
+
+## install docker
+sh -c "$(curl -fsSL https://get.docker.com)"
 
 ## exit the current shell, forcing a re-log in to activate the new custom shell format
 exit
