2cf76d71701642797388105b86211d3ae945ec0b
Drauku <drauku@gmail.com>
2023-05-13 00:56:51 -0500

    fixed .original copy logic, git clone order

--- install-zsh-p10k.sh
+++ install-zsh-p10k.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ## a script to install zsh, oh-my-zsh, powerlevel10k, meslo lgs fonts, zsh plugins
-## docker-ce will be installed, as well as adding a 'docker' user and 'docker' group
 
-## download and run this script with this command: sh -c "$(curl -fsSL https://)"
+## download and run this script with this command: sh -c "$(curl -fsSL https://raw.github.com/drauku/install-zsh-p10k/master/install-zsh-p10k.sh)"
+
+# user warning to exit initial zsh shell to continue this script
+echo "ONCE 'oh-my-zsh' IS FINISHED INSTALLING, TYPE 'exit' TO CONTINUE THIS SCRIPT"
+sleep 5
 
 ## install required apps
 apt update -y && apt upgrade -y && apt install -y curl fontconfig git zsh
@@ -16,19 +19,18 @@ curl -LO https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF
 fc-cache -f -v
 
 ## install oh-my-zsh
-echo "ONCE 'oh-my-zsh' IS FINISHED INSTALLING, TYPE 'exit' TO CONTINUE THIS SCRIPT"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
-
-## download powerlevel10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 ## download zsh-z zsh-autosuggestions zsh-syntax-highlighting plugin
 git clone https://github.com/agkozak/zsh-z "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 
+## download powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k"
+
 ## backup original .zshrc and set theme, plugins, and auto load the .p10k.zsh on terminal session start
-[[ ! -f ~/.zshrc~ ]] || cp ~/.zshrc ~/.zshrc~
+[[ ! -f ~/.zshrc.original ]] && cp ~/.zshrc ~/.zshrc.original #|| printf "\n'%s/.zshrc.original' already exists\n" "$HOME"
 sed -zi 's|ZSH_THEME="robbyrussell"|ZSH_THEME="powerlevel10k/powerlevel10k"|g' ~/.zshrc
 sed -zi 's|plugins=(git)|plugins=(git zsh-z zsh-autosuggestions zsh-syntax-highlighting)|g' ~/.zshrc
 sed -i '1i \ source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"; fi\n' ~/.zshrc
@@ -44,18 +46,8 @@ sed -i '1i # Enable Powerlevel10k instant prompt. Should stay close to the top o
 
 ## backup original .p10k.conf and replace with drauku's custom .p10k.conf
 curl -fs https://raw.githubusercontent.com/Drauku/install-zsh-p10k/main/.p10k.zsh.custom -o ~/.p10k.zsh.custom
-[[ ! -f ~/.p10k.zsh.original ]] || cp ~/.p10k.zsh ~/.p10k.zsh.original
+[[ ! -f ~/.p10k.zsh.original ]] && cp ~/.p10k.zsh ~/.p10k.zsh.original #|| printf "\n'%s/.p10k.zsh.original' already exists\n" "$HOME"
 cp --backup ~/.p10k.zsh.custom ~/.p10k.zsh
-
-# ## add queried user
-# while read -r -p " What 'username' you would like assigned as the first UID/GID? " username; do
-#   # First Last,RoomNumber,WorkPhone,HomePhone
-#   adduser "${username}" --gecos ",,," --disabled-password
-#   echo "${username}:password" | sudo chpasswd
-# done
-
-## install docker
-#sh -c "$(curl -fsSL https://get.docker.com)"
 
 ## exit the current shell, forcing a re-log in to activate the new custom shell format
 exit
